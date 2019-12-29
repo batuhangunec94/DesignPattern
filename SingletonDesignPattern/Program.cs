d3949f24da625d91a7aabe1c7d63474bdf379326
batuhan güneç <proffsar2@gmail.com>
2019-12-29 16:24:10 +0300

    Singleton

---
+++ SingletonDesignPattern/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingletonDesignPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var singletonObject = SingletonObject.CreateSingletonObject();
+            singletonObject.Save();
+        }
+    }
+
+    class SingletonObject
+    {
+        private static SingletonObject _singletonObject;
+
+        public SingletonObject()
+        {
+
+        }
+
+        public static SingletonObject CreateSingletonObject()
+        {
+
+            if (_singletonObject == null)
+            {
+                _singletonObject = new SingletonObject();
+            }
+
+            return _singletonObject;
+        }
+
+        public void Save()
+        {
+            Console.WriteLine("Saved...!");
+            Console.ReadKey();
+        }
+    }
+}
